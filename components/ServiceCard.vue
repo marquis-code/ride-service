2cddc4e47081317d46d310527d6e17b2361f673c
Marquis <abahmarquis@gmail.com>
2024-12-30 23:57:47 -0800

    updated code

--- components/ServiceCard.vue
+++ components/ServiceCard.vue
@@ -1,12 +1,12 @@
 <!-- components/ServiceCard.vue -->
 <template>
-    <main class="my-20">
+    <main class="lg:my-20 my-6">
   <div class="flex justify-center items-center flex-col">
     <div data-aos="flip-left" class="inline-block px-4 py-2.5 mb-6 bg-[#1d4449] rounded-full">
         <span class="text-[#c2f725] font-medium">Our services</span>
       </div>
       
-      <h1 data-aos="fade-up" class="text-2xl max-w-sm mx-auto text-center font-medium text-gray-900 mb-4 -mt-2">
+      <h1 data-aos="fade-up" class="text-2xl lg:text-3xl max-w-md mx-auto text-center font-medium text-gray-900 mb-4 -mt-2">
         Solutions we provide across different aspect of transportation.
       </h1>
       
--- pages/index.vue
+++ pages/index.vue
@@ -52,7 +52,7 @@
             <div class="relative w-full lg:max-w-xl lg:shrink-0 xl:max-w-2xl -mt-20 lg:mt-0">
               <CoreLanding />
             </div>
-            <div class="lg:mt-14 -mt-44 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+            <div class="lg:mt-14 -mt-60 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
               <div class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                 <div class="relative">
                   <img src="@/assets/img/services9.jpg" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg">

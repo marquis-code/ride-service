54734eef83ecd0be753cb7e70a4f0d181f238ddc
marquis-code <abahmarquis@gmail.com>
2025-01-03 05:12:58 -0800

    Update ServiceCard.vue

--- components/ServiceCard.vue
+++ components/ServiceCard.vue
@@ -5,13 +5,13 @@
     <div data-aos="flip-left" class="inline-block px-4 py-2.5 mb-6 bg-[#1d4449] rounded-full">
         <span class="text-[#c2f725] font-medium">Our services</span>
       </div>
-      
+<!--       
       <h1 data-aos="fade-up" class="text-2xl lg:text-3xl max-w-md mx-auto text-center font-medium text-gray-900 mb-4 -mt-2">
         Solutions we provide across different aspect of transportation.
-      </h1>
+      </h1> -->
       
       <p data-aos="fade-up" class="text-gray-600  mb-16 max-w-sm mx-auto text-center">
-        Discover the unparalleled benefits of our services and elevate your travel experience with us today!
+        Our services provide seamless solutions across different aspects of transportation – Experience our benefits by elevating your travel experience with CoMiles!
       </p>
   </div>
       <section class="grid grid-cols-1 lg:grid-cols-2 p-4 lg:p-0 gap-6 max-w-7xl mx-auto">
@@ -60,7 +60,7 @@
           <h3 class="text-2xl font-semibold text-white mb-4">
             {{ item.title ?? 'Nil' }}
           </h3>
-          <p class="text-white/90 mb-8 text-xl">
+          <p class="text-white/90 mb-8 text-base">
             {{ item.description ?? 'Nil' }}
           </p>
   
@@ -84,34 +84,55 @@
     import image2 from '@/assets/img/daily-shuttle.jpeg'
       import image3 from '@/assets/img/vehicle-rental.webp'
         import image4 from '@/assets/img/event-go.webp'
+          import image5 from '@/assets/img/corporate.png'
+           import image6 from '@/assets/img/business-ride.jpg'
+                  import image7 from '@/assets/img/girls.jpg'
+                        import image8 from '@/assets/img/services5.jpg'
+                                        import image9 from '@/assets/img/private.png'
+                                         import image10 from '@/assets/img/place-order.jpg'
+                                          import image11 from '@/assets/img/delivery.jpg'
 
   const servicesInfo = ref([
      {
-        image: image1,
+        image: image10,
         bgCode: '#1d4449',
-        title: 'CoMiles for Business',
-        description: `Help employees get to work safely, comfortably, and on time. Here's an employee perk that actually matters every day.`,
+        title: 'Order, Negotiate, and Go',
+        description: `No more assumptions, you can now determine your ride cost before any trip. Transit to the next big deal with no hidden charges`,
         icon: icon1
      },{
-        image: image2,
+        image: image6,
          bgCode: '#800080',
-        title: 'Daily Shuttle',
-        description: 'Experience the efficiency and convenience of Scheduled Mass-Transit, your dependable solution for seamless, punctual, and eco-friendly commuting.',
+        title: 'CoMiles for Professionals',
+        description: 'At CoMiles, we understand the unique demands of professionals on the move. Whether heading to a business meeting, navigating between work sites, or managing a packed daily schedule, CoMiles provides tailored transport solutions designed with your needs in mind. Enjoy privacy, reliability, and premium comfort during your commute—letting you focus on what matters most: your goals.',
         icon: icon2
      },
      {
-        image: image3,
+        image: image11,
          bgCode: '#1a1b2f',
-        title: 'Vehicle Rental',
-        description: 'Unlock the freedom to go anywhere, anytime with our Vehicle Rental service. Discover a wide range of options to suit your travel needs and explore the world on your terms.',
+        title: 'Daily Errands',
+        description: 'Say goodbye to the stress of managing daily tasks. With CoMiles, running errands becomes a breeze. From grocery shopping and visiting clients to attending appointments, our service ensures you can handle it all efficiently. With affordable rates and flexible booking options, we’re here to save you time and energy daily.',
         icon: icon3
      },
      {
-        image: image4,
-         bgCode: '#008000',
-        title: 'EventGo',
-        description: 'Elevate your event experience with our convenient vehicle booking service. Whether you need transportation for weddings, corporate gatherings, or special occasions, simplify your event logistics with ease.',
-        icon: icon4
+        image: image7,
+         bgCode:'#1a1b2f',
+        title: 'Route Share',
+        description: `Save more, travel smarter! CoMiles Route Share connects you with co-commuters heading your way. Whether you're a driver seeking to offset costs or a passenger looking for affordable rides, this feature promises convenience without compromising on comfort. Stay tuned for the launch of this exciting option to reduce your travel expenses while enjoying a seamless commute.`,
+        icon: icon1
+     },
+     {
+        image: image8,
+         bgCode:  '#800080',
+        title: 'Scheduling',
+        description: `Life doesn’t wait, and neither should you. CoMiles Scheduling lets you plan rides, ensuring your transportation is ready when you are. Perfect for airport runs, important meetings, or any planned travel, this upcoming feature will enhance your convenience and peace of mind.`,
+        icon: icon1
+     },
+     {
+        image: image5,
+         bgCode: '#1d4449',
+        title: 'City-2-City Advantage',
+        description: `Ready to explore new horizons? The City-2-City Advantage by CoMiles offers seamless intercity transport designed for comfort and affordability. Whether for business or leisure, this service will revolutionize the way you travel long distances, making it easier than ever to connect with different cities.`,
+        icon: icon1
      }
   ])
   // No additional logic needed for this component
